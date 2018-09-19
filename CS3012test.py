28c10b20b6ec0fdac0093390ca725277e6587f42
Daniel Browne <dbrowne2@tcd.ie>
2018-09-19 13:26:38 +0100

    added skeleton for tests

---
+++ CS3012test.py
@@ -0,0 +1,14 @@
+import CS3012
+
+# build a sample tree
+
+# test findPath for multiple routes through sample tree
+
+# test lca in straight line tree
+
+# test lca in 3-node tree
+
+# test lca between nodes that aren't on the same tier of the tree
+
+
+
