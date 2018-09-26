f5f5f14858baa54c6ce80893631c329306ac369a
BrowneDaniel <dbrowne2@tcd.ie>
2018-09-26 17:16:06 +0100

    added more tests to the skeleton - actual test code to come

--- CS3012test.py
+++ CS3012test.py
@@ -10,5 +10,14 @@ import CS3012
 
 # test lca between nodes that aren't on the same tier of the tree
 
+# test lca between a node and itself
 
+# test lca between a parent node and its child
 
+# test lca between the root and another node
+
+# test lca between two nodes in an invalid tree
+
+# test lca between non-existent nodes
+
+# test lca between one existing node and one non-existing node
