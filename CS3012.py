780c891147c617097bd0cf9ca36c35c63b33d504
BrowneDaniel <dbrowne2@tcd.ie>
2018-10-13 18:19:27 +0100

    added start of main code

--- CS3012.py
+++ CS3012.py
@@ -1,59 +1,27 @@
-# Lowest Common Ancestor in a Binary Tree
-# Code replicated from method outlined at https://www.geeksforgeeks.org/lowest-common-ancestor-in-a-binary-search-tree/
-
-# A binary tree node
-class Node:
-    # Constructor to create a new binary node
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-
-# Finds the path from root node to given root of the tree.
-# Stores the path in a list path[], returns true if path
-# exists otherwise false
-def findPath(root, path, k):
-    # Base Case
-    if root is None:
-        return False
-
-    # Store this node is path vector. The node will be
-    # removed if not in path from root to k
-    path.append(root.key)
-
-    # See if the k is same as root's key
-    if root.key == k:
-        return True
-
-    # Check if k is found in left or right sub-tree
-    if ((root.left is None and findPath(root.left, path, k)) or
-            (root.right is None and findPath(root.right, path, k))):
-        return True
-
-    # If not present in subtree rooted with root, remove
-    # root from path and return False
-
-    path.pop()
-    return False
-
-# Returns LCA if node n1 , n2 are present in the given
-# binary tree otherwise return -1
+import sys
 
 
-def findLCA(root, n1, n2):
-    # To store paths to n1 and n2 from the root
-    path1 = []
-    path2 = []
+class DAG(object):
 
-    # Find paths from root to n1 and root to n2.
-    # If either n1 or n2 is not present , return -1
-    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
-        return -1
+    # create empty set for nodes for the DAG
+    def __init__(self):
+        self.create_graph()
 
-        # Compare the paths to get the first different value
-    i = 0
-    while i < len(path1) and i < len(path2):
-        if path1[i] != path2[i]:
-            break
-        i += 1
-    return path1[i - 1]
+    # creates empty dict
+    def create_graph(self):
+        self.graph = {}
+
+    # add node to set
+    def add_node(self, node, graph=None):
+        if not graph:
+            graph = self.graph
+
+        if node in graph:
+            return False
+
+        graph[node] = []
+
+    # add edge to set
+    def add_edge(self, ind_node, dep_node, graph=None):
+        if not graph:
+            graph = self.graph
