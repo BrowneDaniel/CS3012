451c00724c5f1fdecb8af0311716ad4e28f49e00
BrowneDaniel <dbrowne2@tcd.ie>
2018-10-14 17:01:00 +0100

    added more main code

--- CS3012.py
+++ CS3012.py
@@ -3,15 +3,12 @@ import sys
 
 class DAG(object):
 
-    # create empty set for nodes for the DAG
     def __init__(self):
-        self.create_graph()
+        self.creategraph()
 
-    # creates empty dict
-    def create_graph(self):
+    def creategraph(self):
         self.graph = {}
 
-    # add node to set
     def add_node(self, node, graph=None):
         if not graph:
             graph = self.graph
@@ -21,7 +18,37 @@ class DAG(object):
 
         graph[node] = []
 
-    # add edge to set
     def add_edge(self, ind_node, dep_node, graph=None):
         if not graph:
             graph = self.graph
+
+        # if both nodes exist in the graph
+        if ind_node in graph and dep_node in graph:
+            graph[ind_node].append(dep_node)
+        else:
+            raise KeyError("One or both nodes do not exist")
+
+        def DFS(self, node_list, graph, node):
+            if not graph[node]:
+                return True
+            else:
+                for child in graph[node]:
+                    if child not in node_list:
+                        node_list.append(child)  # Add to list that stores the route
+                        if not self.DFS(node_list, graph, child):
+                            return False
+                        node_list.remove(child)
+                    else:
+                        return False
+                return True
+
+            # wrapper for DFS function
+
+        def DFSWrapper(self, graph):
+            result = True
+            for node in graph:
+                if not self.DFS([node], graph, node):
+                    result = False
+                    break
+
+            return result
