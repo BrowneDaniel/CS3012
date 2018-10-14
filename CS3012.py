e4b23fe3fb7ed04d9c889a0e7a067caa05d3f9fa
BrowneDaniel <dbrowne2@tcd.ie>
2018-10-14 18:21:25 +0100

    added rest of main code, removed needless comments - need to add more tests for 100% code coverage

--- CS3012.py
+++ CS3012.py
@@ -1,3 +1,4 @@
+# adapted from approach found at https://github.com/neasatang/
 import sys
 
 
@@ -8,6 +9,7 @@ class DAG(object):
 
     def creategraph(self):
         self.graph = {}
+
 
     def add_node(self, node, graph=None):
         if not graph:
@@ -28,27 +30,76 @@ class DAG(object):
         else:
             raise KeyError("One or both nodes do not exist")
 
-        def DFS(self, node_list, graph, node):
-            if not graph[node]:
-                return True
-            else:
-                for child in graph[node]:
-                    if child not in node_list:
-                        node_list.append(child)  # Add to list that stores the route
-                        if not self.DFS(node_list, graph, child):
-                            return False
-                        node_list.remove(child)
-                    else:
+
+    def DFS(self, node_list, graph, node):
+        if not graph[node]:
+            return True
+        else:
+            for child in graph[node]:
+                if child not in node_list:
+                    node_list.append(child) # Add to list that stores the route
+                    if not self.DFS(node_list, graph, child):
                         return False
-                return True
+                    node_list.remove(child)
+                else:
+                    return False
+            return True
 
-            # wrapper for DFS function
+    # wrapper for DFS function
+    def DFSWrapper(self, graph):
+        result = True
+        for node in graph:
+            if not self.DFS([node], graph, node):
+                result = False
+                break
 
-        def DFSWrapper(self, graph):
-            result = True
-            for node in graph:
-                if not self.DFS([node], graph, node):
-                    result = False
-                    break
+        return result
 
-            return result
+    # wrapper for finding LCA using DFS
+    def LCA_DFS_Wrapper(self, graph, nodeA, nodeB):
+        global node_A_list
+        node_A_list = []
+        global node_B_list
+        node_B_list = []
+
+        # gets the node routes for each node and stores them in a list
+        for node in graph:
+            self.LCA_DFS([node], graph, node, 1, nodeA)
+            self.LCA_DFS([node], graph, node, 2, nodeB)
+
+        lowest_count = sys.maxsize
+        for itemA in node_A_list:
+            for itemB in node_B_list:
+                for index, node1 in enumerate(reversed(itemA)):
+                    count = index
+                    for node2 in reversed(itemB):
+                        if node1 == node2 and count < lowest_count: # LCA is the one with the lowest count
+                            LCANode = node2
+                            return LCANode
+
+                        count += 1
+
+    # calculates the DFS for an LCA node
+    def LCA_DFS(self, node_list, graph, node, index, end_node):
+        if node == end_node:
+
+            # distinguish between the two routes using index
+            if index == 1:
+                node_A_list.append(node_list[:])
+            elif index == 2:
+                node_B_list.append(node_list[:])
+            return True
+
+        if not graph[node]:
+            return True
+
+        else:
+            for child in graph[node]:
+                if child not in node_list:
+                    node_list.append(child)
+                    if not self.LCA_DFS(node_list, graph, child, index, end_node):
+                        return False
+                    node_list.remove(child)
+                else:
+                    return False
+            return True
